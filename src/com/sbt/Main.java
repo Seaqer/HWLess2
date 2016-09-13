e2c4b25f4508c439c4dbb0ffba39594a7e91e99c
Tema <d@g.c>
2016-09-13 12:46:17 +0300

    Initial commit

---
+++ src/com/sbt/Main.java
@@ -0,0 +1,23 @@
+package com.sbt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Person manIgor = new Person(true,"Igor");
+        Person womanLiza = new Person(false,"Liza");
+        Person womanElise = new Person(false,"Elise");
+        Person manEgor = new Person(true,"Elise");
+        manIgor.marry(womanLiza);
+        manIgor.marry(womanLiza);
+        womanElise.marry(manEgor);
+        manIgor.marry(womanElise);
+        manEgor.marry(manIgor);
+    }
+}
+
+
+
+
+
+
