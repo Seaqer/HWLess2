9d9fa6836308b5168a1dd4d44cd929ad9a5fa7b5
Tema <d@g.c>
2016-09-16 13:40:25 +0300

    Changed 2

--- src/com/sbt/Main.java
+++ src/com/sbt/Main.java
@@ -10,24 +10,25 @@ public class Main {
         Person manEgor = new Person(true, "Elise");
 
         if (!womanElise.marry(manEgor)) {
-            System.out.println("ERROR: !womanElise.marry(manEgor)");
+            System.out.println("ERROR: womanElise.marry(manEgor) not married");
         }
 
         if (!manIgor.marry(womanLiza)) {
-            System.out.println("ERROR: !manIgor.marry(womanLiza)");
+            System.out.println("ERROR: manIgor.marry(womanLiza) not married");
         }
 
         if (manIgor.marry(womanLiza)) {
-            System.out.println("ERROR: manIgor.marry(womanLiza)");
+            System.out.println("ERROR: manIgor.marry(womanLiza) remarried");
         }
 
         if (!manIgor.marry(womanElise)) {
-            System.out.println("ERROR: !manIgor.marry(womanElise)");
+            System.out.println("ERROR: manIgor.marry(womanElise) not married");
         }
 
         if (manEgor.marry(manIgor)) {
-            System.out.println("ERROR: manEgor.marry(manIgor)");
+            System.out.println("ERROR: manEgor.marry(manIgor) married");
         }
+
         if (manIgor.marry(null)) {
             System.out.println("ERROR: manIgor.marry(null)");
         }
